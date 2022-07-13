e399e9deb0252aab420ced0e1df5cded083d17fa
esborisova <sk.borisova@gmail.com>
2022-07-13 13:57:03 +0200

    remove duplicates

--- src/pipelines/BERT.py
+++ src/pipelines/BERT.py
@@ -3,12 +3,16 @@ import sys
 sys.path.append("../scripts")
 from clean_tweets import clean_text
 import pandas as pd
+import os
 import regex as re
 from pysentimiento.preprocessing import preprocess_tweet
 from pysentimiento import create_analyzer
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
 
-df = pd.read_pickle(str(sys.argv[1]))
+root_dir = "../../split_data/"
+file_path = os.path.join(root_dir, str(sys.argv[1]))
+df = pd.read_pickle(file_path)
 df = df.filter(["id", "created_at", "from_user_id", "text"])
 
 tokenizer = AutoTokenizer.from_pretrained("pysentimiento/robertuito-sentiment-analysis")
@@ -16,7 +20,11 @@ model = AutoModelForSequenceClassification.from_pretrained(
     "pysentimiento/robertuito-sentiment-analysis"
 )
 
-# torch.cuda_set_device(1)
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+else:
+    device = torch.device("cpu")
+
 analyzer_sent = create_analyzer(task="sentiment", lang="en")
 analyzer_em = create_analyzer(task="emotion", lang="en")
 
@@ -36,4 +44,6 @@ emotion_predictions = analyzer_em.predict(tweets)
 emotion_tags = [prediction.output for prediction in emotion_predictions]
 df["BERT_emot"] = emotion_tags
 
-df.to_pickle(f"../../split_data/BERT_{str(sys.argv[1])}.pkl")
+df.to_pickle(f"../../split_data/BERT_{str(sys.argv[1])}")
+df0 = df.drop_duplicates(subset=["text"], keep="first")
+df0.to_pickle(f"../../split_data/BERT_{str(sys.argv[1])}_no_rts.pkl")
